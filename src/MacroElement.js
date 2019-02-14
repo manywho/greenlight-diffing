fe93360ff90e13e3d31ec3b83762d605e5dc2f13
Jacek Wojciechowski <jacek_wojciechowski@dell.com>
2019-02-14 16:28:12 +0000

    Enabling Who in MacroElement

--- src/MacroElement.js
+++ src/MacroElement.js
@@ -4,6 +4,14 @@ import Who from './Who';
 
 function handleCustomElement(node, path, key, item, rootPath, snapshotA, snapshotB) {
 
+    // console.log("handleCustomElement, path=" + path + ", key=" + key + ", rootPath=" + rootPath);
+
+    if (item != null && (rootPath.endsWith(".whoOwner") || rootPath.endsWith(".whoCreated") || rootPath.endsWith(".whoModified"))) {
+
+        node.element = <Who who={item}/>;
+        return true;
+    }
+
     return false;
 }
 
